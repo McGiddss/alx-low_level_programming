46a865b726589b7ab6081eedc406281df1c3f4ec
root <Gideon>
2022-12-09 15:39:09 -0800

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,8 @@ int main(void)
 			for (ones = '0'; ones <= '9'; ones++)
 			{
 				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*eliminates repitition*/
-				{	putchar(hundreds);
+				{	
+					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
 					if (!(ones == '9' && hundreds == '7' && tens == '8')) /*adds commas and spaces*/
