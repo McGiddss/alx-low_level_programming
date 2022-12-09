5f740477fab72957cce93796f08c7bcf4e00c228
root <Gideon>
2022-12-09 13:00:24 -0800

    printnumberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Return: Always 0 (success)
- */
+* main - Entry point 
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-	int c;
-	
+	int c;	
 	c = 0;
 	while
 		(c < 10) {
