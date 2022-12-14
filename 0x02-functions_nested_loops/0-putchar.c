841c9876d781c25fab053a3e2003ea1505a5328b
root <Gideon>
2022-12-14 08:06:17 -0800

    taskput

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry Point
  *
- * Return: Always 
+ * Return: Always
  */
 
 int main(void)
 {
-	
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
