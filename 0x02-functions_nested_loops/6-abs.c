05bde42285bd4ff11d8d4af8a03ed6a035a4e8b9
root <Gideon>
2022-12-14 11:22:32 -0800

    absf

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs -> Function that computes
+ *
+ * Return: y if u > 0, 0 if y == 0
+ */
+
+int _abs(int y)
+{
+	if (y > 0);
+	{
+		return (y);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-y);
+	}
+}
