4ce6a8d33eb363a05b3e78788ba4dbd7ea3ca9b8
root <Gideon>
2022-12-14 13:05:25 -0800

    elevenm

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,16 +18,16 @@ void print_to_98(int n)
 		while (n < 98)
 		{
 			printf("%d, ", n);
-			n++
+			n++;
 		}
-		else if (n > 98)
-		{
-			while (n > 98)
-			{
-				printf("%d, ", n);
-				n--;
-			}
-		}
-		printf("98\n");
 	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
