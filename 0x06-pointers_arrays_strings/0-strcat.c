404c745f5b17427914211ca8d25f2b72b3133aef
root <Gideon>
2022-12-22 06:42:48 -0800

    strcatt

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,13 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the x
- *          null byte, to the end of the string pointed to by @dest.
+ * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  * Return: A pointer to the destination string @dest.
  */
 
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -16,7 +16,7 @@ char *_strcat(char *dest, const char *src)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++
+		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
