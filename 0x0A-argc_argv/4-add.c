c7c859d472f082c82fdca724e0d0a939018450cc
root <Gideon>
2023-01-11 13:07:21 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+#include "main.h"
 /**
-	 * check_num - check - string there are digit
+	 * check_num - check string there are digit
 	 * @str: array str
 	 *
 	 * Return: Always 0 (Success)
@@ -32,53 +32,36 @@
 	}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
 
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	int main(int argc, char *argv[])
-
-
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
+		if (check_num(argv[count]))
 		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-
-			count++;
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
-
-
-		printf("%d\n", sum); /*print sum*/
-
-
-		return (0);
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
+	printf("%d\n", sum); /*print sum*/
+	return (0);
+}
